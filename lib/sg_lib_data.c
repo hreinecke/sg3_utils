c6737e9da997d3dac104a489a8c79459ba43e099
Douglas Gilbert <dgilbert@interlog.com>
2009-08-24 23:11:14 +0000

    sg_persist: tranport specific transportIDs, read transportIDs from file; add Progress indication sense data descriptor
    git-svn-id: https://svn.bingwo.ca/repos/sg3_utils/trunk@290 6180dd3e-e324-4e3e-922d-17de1ae2f315

--- lib/sg_lib_data.c
+++ lib/sg_lib_data.c
@@ -37,7 +37,7 @@
 #endif
 
 
-const char * sg_lib_version_str = "1.52 20090816";    /* spc-4 rev 20+ */
+const char * sg_lib_version_str = "1.53 20090824";    /* spc-4 rev 20+ */
 
 struct sg_lib_value_name_t sg_lib_normal_opcodes[] = {
     {0, 0, "Test Unit Ready"},
