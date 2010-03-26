eb429da3bf7cf2b1e6b0f9837074a2b1d2f8d125
Douglas Gilbert <dgilbert@interlog.com>
2010-03-26 22:49:04 +0000

    add examples bsg_queue_tst, wire Q_AT_HEAD flag
    git-svn-id: svn://localhost/trunk@332 6180dd3e-e324-4e3e-922d-17de1ae2f315

--- examples/bsg_queue_tst.c
+++ examples/bsg_queue_tst.c
@@ -8,6 +8,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+/* If the following fails the Linux kernel is probably too old */
+#include <linux/bsg.h>
+
 
 #include "sg_lib.h"
 #include "sg_io_linux.h"
@@ -23,7 +26,7 @@
 *  the Free Software Foundation; either version 2, or (at your option)
 *  any later version.
 
-   Invocation: sg_queue_tst [-t] <sg_device>
+   Invocation: bsg_queue_tst [-t] <bsg_device>
         -t      queue at tail
 
    Version 0.90 (20100324)
@@ -37,25 +40,25 @@
 
 #define EBUFF_SZ 256
 
-#ifndef SG_FLAG_Q_AT_TAIL
-#define SG_FLAG_Q_AT_TAIL 0x10
+#ifndef BSG_FLAG_Q_AT_TAIL
+#define BSG_FLAG_Q_AT_TAIL 0x10
 #endif
 
-#ifndef SG_FLAG_Q_AT_HEAD
-#define SG_FLAG_Q_AT_HEAD 0x20
+#ifndef BSG_FLAG_Q_AT_HEAD
+#define BSG_FLAG_Q_AT_HEAD 0x20
 #endif
 
 
 int main(int argc, char * argv[])
 {
-    int sg_fd, k, ok;
+    int bsg_fd, k, ok;
     unsigned char inqCmdBlk[INQ_CMD_LEN] =
                                 {0x12, 0, 0, 0, INQ_REPLY_LEN, 0};
     unsigned char sdiagCmdBlk[SDIAG_CMD_LEN] =
                                 {0x1d, 0, 0, 0, 0, 0};
     unsigned char inqBuff[16][INQ_REPLY_LEN];
-    sg_io_hdr_t io_hdr[16];
-    sg_io_hdr_t rio_hdr;
+    struct sg_io_v4 io_hdr[16];
+    struct sg_io_v4 rio_hdr;
     char * file_name = 0;
     char ebuff[EBUFF_SZ];
     unsigned char sense_buffer[16][SENSE_BUFFER_LEN];
@@ -78,88 +81,91 @@ int main(int argc, char * argv[])
         }
     }
     if (0 == file_name) {
-        printf("Usage: 'sg_queue_tst [-t] <sg_device>'\n"
+        printf("Usage: 'bsg_queue_tst [-t] <bsg_device>'\n"
                "where:\n      -t   queue_at_tail (def: q_at_head)\n");
         return 1;
     }
 
     /* An access mode of O_RDWR is required for write()/read() interface */
-    if ((sg_fd = open(file_name, O_RDWR)) < 0) {
+    if ((bsg_fd = open(file_name, O_RDWR)) < 0) {
         snprintf(ebuff, EBUFF_SZ,
-                 "sg_queue_tst: error opening file: %s", file_name);
+                 "bsg_queue_tst: error opening file: %s", file_name);
         perror(ebuff);
         return 1;
     }
 
     for (k = 0; k < 16; ++k) {
         /* Prepare INQUIRY command */
-        memset(&io_hdr[k], 0, sizeof(sg_io_hdr_t));
-        io_hdr[k].interface_id = 'S';
+        memset(&io_hdr[k], 0, sizeof(struct sg_io_v4));
+        io_hdr[k].guard = 'Q';
         /* io_hdr[k].iovec_count = 0; */  /* memset takes care of this */
-        io_hdr[k].mx_sb_len = (unsigned char)sizeof(sense_buffer);
         if (0 == (k % 3)) {
-            io_hdr[k].cmd_len = sizeof(sdiagCmdBlk);
-            io_hdr[k].cmdp = sdiagCmdBlk;
-            io_hdr[k].dxfer_direction = SG_DXFER_NONE;
+            io_hdr[k].request_len = sizeof(sdiagCmdBlk);
+            io_hdr[k].request = (uint64_t)(long)sdiagCmdBlk;
         } else {
-            io_hdr[k].cmd_len = sizeof(inqCmdBlk);
-            io_hdr[k].cmdp = inqCmdBlk;
-            io_hdr[k].dxfer_direction = SG_DXFER_FROM_DEV;
-            io_hdr[k].dxfer_len = INQ_REPLY_LEN;
-            io_hdr[k].dxferp = inqBuff[k];
+            io_hdr[k].request_len = sizeof(inqCmdBlk);
+            io_hdr[k].request = (uint64_t)(long)inqCmdBlk;
+            io_hdr[k].din_xfer_len = INQ_REPLY_LEN;
+            io_hdr[k].din_xferp = (uint64_t)(long)inqBuff[k];
         }
-        io_hdr[k].sbp = sense_buffer[k];
-        io_hdr[k].mx_sb_len = SENSE_BUFFER_LEN;
+        io_hdr[k].response = (uint64_t)(long)sense_buffer[k];
+        io_hdr[k].max_response_len = SENSE_BUFFER_LEN;
         io_hdr[k].timeout = 20000;     /* 20000 millisecs == 20 seconds */
-        io_hdr[k].pack_id = k;
+        io_hdr[k].usr_ptr = k;
         /* default is to queue at head (in SCSI mid level) */
         if (q_at_tail)
-            io_hdr[k].flags |= SG_FLAG_Q_AT_TAIL;
+            io_hdr[k].flags |= BSG_FLAG_Q_AT_TAIL;
         else
-            io_hdr[k].flags |= SG_FLAG_Q_AT_HEAD;
-        /* io_hdr[k].usr_ptr = NULL; */
+            io_hdr[k].flags |= BSG_FLAG_Q_AT_HEAD;
 
-        if (write(sg_fd, &io_hdr[k], sizeof(sg_io_hdr_t)) < 0) {
-            perror("sg_queue_tst: sg write error");
-            close(sg_fd);
+        if (write(bsg_fd, &io_hdr[k], sizeof(struct sg_io_v4)) < 0) {
+            perror("bsg_queue_tst: bsg write error");
+            close(bsg_fd);
             return 1;
         }
     }
     /* sleep(3); */
     for (k = 0; k < 16; ++k) {
-        memset(&rio_hdr, 0, sizeof(sg_io_hdr_t));
-        rio_hdr.interface_id = 'S';
-        if (read(sg_fd, &rio_hdr, sizeof(sg_io_hdr_t)) < 0) {
-            perror("sg_queue_tst: sg read error");
-            close(sg_fd);
+        memset(&rio_hdr, 0, sizeof(struct sg_io_v4));
+        rio_hdr.guard = 'Q';
+        if (read(bsg_fd, &rio_hdr, sizeof(struct sg_io_v4)) < 0) {
+            perror("bsg_queue_tst: bsg read error");
+            close(bsg_fd);
             return 1;
         }
         /* now for the error processing */
         ok = 0;
-        switch (sg_err_category3(&rio_hdr)) {
-        case SG_LIB_CAT_CLEAN:
+        if (0 == rio_hdr.device_status)
             ok = 1;
-            break;
-        case SG_LIB_CAT_RECOVERED:
-            printf("Recovered error, continuing\n");
-            ok = 1;
-            break;
-        default: /* won't bother decoding other categories */
-            sg_chk_n_print3("command error", &rio_hdr, 1);
-            break;
+        else {
+            switch (sg_err_category_sense((unsigned char *)(long)rio_hdr.response,
+                    rio_hdr.response_len)) {
+            case SG_LIB_CAT_CLEAN:
+                ok = 1;
+                break;
+            case SG_LIB_CAT_RECOVERED:
+                printf("Recovered error, continuing\n");
+                ok = 1;
+                break;
+            default: /* won't bother decoding other categories */
+                sg_print_sense("command error",
+                               (unsigned char *)(long)rio_hdr.response,
+                               rio_hdr.response_len, 1);
+                break;
+            }
         }
 
         if (ok) { /* output result if it is available */
             /* if (0 == rio_hdr.pack_id) */
-            if (0 == (rio_hdr.pack_id % 3))
-                printf("SEND DIAGNOSTIC %d duration=%u\n", rio_hdr.pack_id,
+            if (0 == (rio_hdr.usr_ptr % 3))
+                printf("SEND DIAGNOSTIC %d duration=%u\n", (int)rio_hdr.usr_ptr,
                        rio_hdr.duration);
             else
-                printf("INQUIRY %d duration=%u\n", rio_hdr.pack_id,
+                printf("INQUIRY %d duration=%u\n", (int)rio_hdr.usr_ptr,
                        rio_hdr.duration);
         }
     }
 
-    close(sg_fd);
+    close(bsg_fd);
     return 0;
 }
