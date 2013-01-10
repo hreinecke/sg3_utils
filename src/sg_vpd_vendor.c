4c9821a8b70c3a07edd3ea657ddbc805128e3793
Hannes Reinecke <hare@suse.de>
2013-01-10 09:54:08 +0100

    sg_vpd_vendor: Decode RDAC VPD page 0xc1
    Decode RDAC Firmware version page.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- src/sg_vpd_vendor.c
+++ src/sg_vpd_vendor.c
@@ -48,6 +48,7 @@
 #define VPD_V_UPR_EMC  0xc0
 #define VPD_V_HVER_RDAC  0xc0
 #define VPD_V_DATC_SEA  0xc1
+#define VPD_V_FVER_RDAC  0xc1
 #define VPD_V_JUMP_SEA 0xc2
 #define VPD_V_SVER_RDAC 0xc2
 #define VPD_V_DEV_BEH_SEA 0xc3
@@ -88,6 +89,7 @@ static struct svpd_values_name_t vendor_vpd_pg[] = {
      "(RDAC)"},
     {VPD_V_FEAT_RDAC, 1, -1, 1, "feat", "Feature Parameters (RDAC)"},
     {VPD_V_FIRM_SEA, 0, -1, 1, "firm", "Firmware numbers (Seagate)"},
+    {VPD_V_FVER_RDAC, 1, -1, 1, "fver", "Firmware version (RDAC)"},
     {VPD_V_HP3PAR, 2, -1, 1, "hp3par", "Volume information (HP/3PAR)"},
     {VPD_V_HVER_RDAC, 3, -1, 1, "hver", "Hardware version (RDAC)"},
     {VPD_V_JUMP_SEA, 0, -1, 1, "jump", "Jump setting (Seagate)"},
@@ -438,6 +440,49 @@ decode_rdac_vpd_c0(unsigned char * buff, int len)
 }
 
 static void
+decode_rdac_vpd_c1(unsigned char * buff, int len)
+{
+    int i, n, v, r, m, p, d, y, num_part;
+    char part[5];
+
+    if (len < 3) {
+        fprintf(stderr, "Firmware Version VPD page length too "
+                "short=%d\n", len);
+        return;
+    }
+    if (buff[4] != 'f' && buff[5] != 'w' && buff[6] != 'r') {
+        fprintf(stderr, "Invalid page identifier %c%c%c%c, decoding "
+                "not possible.\n" , buff[4], buff[5], buff[6], buff[7]);
+        return;
+    }
+    printf("  Firmware Version: %x.%x.%x\n", buff[8], buff[9], buff[10]);
+    printf("  Firmware Date: %02d/%02d/%02d\n", buff[11], buff[12], buff[13]);
+
+    num_part = (len - 12) / 16;
+    n = 16;
+    printf("  Partitions: %d\n", num_part);
+    for (i = 0; i < num_part; i++) {
+        memset(part,0, 5);
+        memcpy(part, &buff[n], 4);
+        printf("    Name: %s\n", part);
+        n += 4;
+        v = buff[n++];
+        r = buff[n++];
+        m = buff[n++];
+        p = buff[n++];
+        printf("    Version: %d.%d.%d.%d\n", v, r, m, p);
+        m = buff[n++];
+        d = buff[n++];
+        y = buff[n++];
+        printf("    Date: %d/%d/%d\n", m, d, y);
+
+        n += 5;
+    }
+
+    return;
+}
+
+static void
 decode_rdac_vpd_c2(unsigned char * buff, int len)
 {
     int i, n, v, r, m, p, d, y, num_part;
@@ -739,6 +784,44 @@ svpd_decode_vendor(int sg_fd, int num_vpd, int subvalue, int maxlen,
                 decode_vpd_c0_hp3par(rsp_buff, len);
             else if (3 == subvalue)
                 decode_rdac_vpd_c0(rsp_buff, len);
+            else
+                dStrHex((const char *)rsp_buff, len, 0);
+            return 0;
+        }
+        break;
+    case VPD_V_FVER_RDAC:
+        if ((! do_raw) && (! do_quiet))
+            printf("%s VPD Page:\n", name);
+        res = sg_ll_inquiry(sg_fd, 0, 1, num_vpd, rsp_buff, alloc_len, 1,
+                            verbose);
+        if (0 == res) {
+            len = rsp_buff[3] + 4;
+            if (num_vpd != rsp_buff[1]) {
+                fprintf(stderr, "invalid VPD response; probably not "
+                        "supported\n");
+                return SG_LIB_CAT_MALFORMED;
+            }
+            if (len > alloc_len) {
+                if ((0 == maxlen) && (len < MX_ALLOC_LEN)) {
+                    res = sg_ll_inquiry(sg_fd, 0, 1, num_vpd, rsp_buff, len,
+                                        1, verbose);
+                    if (res) {
+                        fprintf(stderr, "fetching 0xc1 page "
+                                "(alloc_len=%d) failed\n", len);
+                        return res;
+                    }
+                } else {
+                    fprintf(stderr, ">>> warning: response length (%d) "
+                            "longer than requested (%d)\n", len, alloc_len);
+                    len = alloc_len;
+                }
+            }
+            if (do_raw)
+                dStrRaw((const char *)rsp_buff, len);
+            else if (do_hex)
+                dStrHex((const char *)rsp_buff, len, 0);
+            else if (1 == subvalue)
+                decode_rdac_vpd_c1(rsp_buff, len);
             else
                 dStrHex((const char *)rsp_buff, len, 0);
             return 0;
