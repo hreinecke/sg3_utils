424c0f6bd5ffbac95d4c3695e4145bddea87176b
Doug Gilbert <dgilbert@interlog.com>
2018-04-05 18:44:15 +0000

    sg_lib: add sg_get_big_endian(), sg_set_big_endian()
    git-svn-id: svn://localhost/trunk@767 6180dd3e-e324-4e3e-922d-17de1ae2f315

--- lib/sg_lib_data.c
+++ lib/sg_lib_data.c
@@ -17,7 +17,7 @@
 #include "sg_lib_data.h"
 
 
-const char * sg_lib_version_str = "2.42 20180402";/* spc5r19, sbc4r15 */
+const char * sg_lib_version_str = "2.43 20180405";/* spc5r19, sbc4r15 */
 
 
 /* indexed by pdt; those that map to own index do not decay */
